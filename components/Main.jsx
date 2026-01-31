bb01b9f94460a542d11f4285d25be8ae00eda19b
Sandraa16012007 <sandrarosaprince@gmail.com>
2026-02-01 01:02:26 +0530

    added basic ui

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,28 +1,39 @@
+import {useState} from "react";
+
 export default function Main() {
 
-    const ingredients = ["cheese", "tomato", "basil"];
+    const [ingredients, setIngredients] = useState([]);
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient.charAt(0).toUpperCase() + ingredient.substring(1).toLowerCase()}</li>
     ));
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log("Form submitted");
-        const newIngredient = event.target.ingredient.value;
-        ingredients.push(newIngredient);
-        console.log(ingredients);
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient").trim();
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
     }
 
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="recipe-form">
+            <form action={addIngredient} className="recipe-form">
                 <input type="text" placeholder="e.g. cheese" name="ingredient" className="ingredient-input" />
                 <button type="submit" className="add-button">+ Add Ingredients</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+
+                <ul className="ingredients-list">
+                    {ingredientsListItems}
+                </ul>
+
+                <div className="generate-recipe-container">
+                    <h3>Ready for a recipe?</h3>
+                    <p>Click the button below to generate a recipe using your ingredients.</p>
+                    <button className="generate-recipe-button">Generate Recipe</button>
+                </div>
+            </section>}
+
         </main>
     )
 }
